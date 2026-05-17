3dfcbae919156b82651ce6468c6f12e8d802e51e
Matt Vinall <matt.vinall7@gmail.com>
2026-05-17 13:07:33 -0400

    feat: enable View Transitions API

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  experimental: {
+    viewTransition: true
+  },
   typescript: {
     strict: true,
     typeCheck: true,
